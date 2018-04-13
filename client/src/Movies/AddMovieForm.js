c66d2ac4572915df3dc73fb56b1b732767c4aa1f
JohnCoronel <john.m.coronel@gmail.com>
2018-04-12 21:01:23 -0700

    added autosugest search component

--- client/src/Movies/AddMovieForm.js
+++ client/src/Movies/AddMovieForm.js
@@ -1,53 +1,81 @@
 import React from 'react';
 import axios from 'axios';
+import Autosuggest from 'react-autosuggest';
 
+
+const API_KEY = 'aa71be19e3f94c1686212b7b46b28de9'
+//const SEARCH_MOVIE_URL = `http://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
+const CREDITS_URL = 'https://api.themoviedb.org/3/movie/244786/credits?api_key=' + API_KEY
+const SEARCH_MOVIE_URL = 'http://www.omdbapi.com/?apikey=ca6d8933&type=movie&s='
+
+const createQuery = id => {
+    return 'https://api.themoviedb.org/3/movie/' + id + '/credits?api_key=' + API_KEY
+}
+const getSuggestionValue = suggestion => suggestion.Title;
+
+
+const renderSuggestion = suggestion => (
+    <div>
+        {suggestion.Title}
+        </div>
+)
 class AddMovieForm extends React.Component{
     constructor(){
         super();
         this.state = {
-            title: '',
-            metascore: '',
-            stars: '',
-            director: ''
+           value: '',
+           suggestions:[]
+
         }
     }
-
-    
-
-    handleOnSubmit  = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3333/api/movies',{
-            title:this.state.title,
-            metascore: parseInt(this.state.metascore),
-            stars: this.state.stars.split(','),
-            director: this.state.director
-        })
-        .then(() => {
-            window.location = '/'
+    asyncFetch = (value) =>{
+        axios.get(SEARCH_MOVIE_URL+value)
+        .then(response => {
+            if( response.data.Response === 'True'){
+                this.setState({suggestions:response.data.Search})
+            }
+            else{
+                this.setState({suggestions:[]})
+            }
         })
     }
-
-    handleInputChange = e => {
+    onSuggestionsFetchRequested = ({value}) => {
+        this.asyncFetch(value)
        
-        const {target}  = e
-        let obj = {}
-        obj[target.name] = target.value
-        console.log(obj)
-        this.setState(obj)
     }
 
+    onChange = (event) => {
+        this.setState({
+            value:event.target.value
+        })
+    }
+
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        })
+    }
+
+  
 
     render(){
+        const {value,suggestions} = this.state;
+
+        const inputProps = {
+            placeholder: 'Enter a movie',
+            value,
+            onChange: this.onChange
+        }
         return (
-            <div>
-                <form id = "addMovie" onSubmit = {this.handleOnSubmit}>
-                <input onChange = {this.handleInputChange} value = {this.state.title} type = "text" name = "title" placeholder = "title"/>
-                <input onChange = {this.handleInputChange} value = {this.state.metascore} type = "text" name = "metascore" placeholder = "metascore"/>
-                <input onChange = {this.handleInputChange} value = {this.state.stars} type = "text" name = "stars" placeholder = "stars - separate by value"/>
-                <input onChange = {this.handleInputChange} value = {this.state.director} type = "text" name = "director" placeholder = "director"/>
-                <button type ="submit" form ="addMovie">Send</button>
-                </form>
-            </div>
+            <Autosuggest
+                suggestions = {suggestions}
+                onSuggestionsFetchRequested = {this.onSuggestionsFetchRequested}
+                onSuggestionsClearRequested = {this.onSuggestionsClearRequested}
+                getSuggestionValue = {getSuggestionValue}
+                renderSuggestion = {renderSuggestion}
+                inputProps = {inputProps}
+                />            
         )
     }
 }
